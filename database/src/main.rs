07944ae1da57b9c99070193857e0737877aaeca0
steffen <stro889002@aol.com>
2023-03-20 22:37:04 +0100

    start

--- database/src/main.rs
+++ database/src/main.rs
@@ -1,5 +1,17 @@
+use postgres::{Client, Error, NoTls};
 
+
+fn database_query(sql: &str) -> Result<() Error> {
+    let mut client = Client::connect("postgresql://postgres:postgres@localhost:5432/biene", NoTls)?;
+    for row in client.query(sql, &[])? {
+        let id: &str = row.get(0);
+        let name: &str = row.get(1);
+        let yard: &str = row.get(2);
+        println!("id: {} name: {} yard: {}", id, name, yard);
+    };
+    Ok(())
+}
 
 fn main() {
-    println!("Hello, world!");
+    database_query("select * from hives;").unwrap();
 }
