39f0277632a9c014ea01443ab0ce6fda749d4fa2
steffen <stro889002@aol.com>
2023-03-21 16:41:50 +0100

    start

--- database/src/main.rs
+++ database/src/main.rs
@@ -1,20 +1,31 @@
-use postgres::{Client, Error, NoTls};
-
+use postgres::{Client, Error, types, NoTls};
 
 fn database_query(sql: &str) -> Result<i32, Error> {
     let mut client = Client::connect("postgresql://postgres:postgres@localhost:5432/biene", NoTls)?;
-
     for row in client.query(sql, &[])? {
         let id: &str = row.get(0);
         let name: &str = row.get(1);
         let yard: &str = row.get(2);
         println!("id: {} name: {} yard: {}", id, name, yard);
     };
-
-    Ok(12)
+    Ok(12)  // return a test value
 }
+
+fn database_execute(sql: &str) -> Result<&str, Error> {
+    let mut client = Client::connect("postgresql://postgres:postgres@localhost:5432/biene", NoTls)?;
+    for row in client.query(sql, &[])? {
+        for (col_idx, _col) in row.columns().iter().enumerate() {
+            let _val: &str = row.get(col_idx);
+            print!("{} ", _val);
+        }
+    };
+    Ok("13")   // return a test value
+}
+
 
 fn main() {
     let test = database_query("select * from hives;").unwrap();
-    println!("{}", test)
+    println!("{}", test);
+    let test2 = database_execute("select * from hives;").unwrap();
+    println!("{}", test2)
 }
