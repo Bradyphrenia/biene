c68856aa26390ce4f5238bf115fb26bc492684d7
steffen <stro889002@aol.com>
2023-03-21 16:55:41 +0100

    start

---
+++ database/create_table.sql
@@ -0,0 +1,20 @@
+-- auto-generated definition
+create table hives
+(
+    "Hive ID"        varchar(255) not null
+        primary key,
+    "Hive Name"      varchar(255),
+    "Yard Name"      varchar(255),
+    "Yard ID"        varchar(255),
+    "Date Created"   varchar(255),
+    "Description"    varchar(255),
+    "Lat"            varchar(255),
+    "Lng"            varchar(255),
+    "Strength"       varchar(255),
+    "Bee Source"     varchar(255),
+    "Frame Count"    varchar(255),
+    "Hive State"     varchar(255),
+    "Component Type" varchar(255),
+    "Sun Exposure"   varchar(255),
+    "Components"     varchar(255)
+);
