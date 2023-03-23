32f62d510fe471fe4b91c01f692dbf90e87b65ac
steffen <stro889002@aol.com>
2023-03-23 19:51:05 +0100

    refactor: funcion to fetch one data set from table volk :-)

--- database/src/main.rs
+++ database/src/main.rs
@@ -1,31 +1,5 @@
 use postgres::{Client, Error, NoTls, Row};
 
-fn database_query(sql: &str) -> Result<i32, Error> {
-    let mut client = Client::connect("postgresql://postgres:postgres@localhost:5432/biene", NoTls)?;
-    for row in client.query(sql, &[])? {
-        let id: &str = row.get(0);
-        let name: &str = row.get(1);
-        let yard: &str = row.get(2);
-        println!("id: {} name: {} yard: {}", id, name, yard);
-    }
-    Ok(12) // return a test value
-}
-
-fn database_execute(sql: &str) -> Result<&str, Error> {
-    let mut client = Client::connect("postgresql://postgres:postgres@localhost:5432/biene", NoTls)?;
-    for row in client.query(sql, &[])? {
-        for (col_idx, _col) in row.columns().iter().enumerate() {
-            let _val: &str = match row.get(col_idx) {
-                //now it works with pattern matching :-)
-                Some(val) => val,
-                None => "",
-            };
-            print!("{} ", _val);
-        }
-    }
-    Ok("13") // return a test value
-}
-
 pub struct Volk {
     pub id: i32,
     pub volk: String,
@@ -63,34 +37,15 @@ impl From<Row> for Volk {
     }
 }
 
-fn database_test(sql: &str) -> Result<Volk, Error> {
+fn database_fetchone(sql: &str) -> Result<Volk, Error> {
     let mut client = Client::connect("postgresql://postgres:postgres@localhost:5432/biene", NoTls)?;
-    let mut result: Volk = Volk {
-        id: 0,
-        volk: "".to_string(),
-        nummer: 0,
-        koenigin: "".to_string(),
-        erstellt: "".to_string(),
-        aufgeloest: "".to_string(),
-        typ: "".to_string(),
-        raehmchenmass: "".to_string(),
-        stand: "".to_string(),
-    };
-    for row in client.query(sql, &[])? {
-        let volk = Volk::from(row);
-        result = volk
-    }
-    Ok(result) // return a strukt
+    let row = client.query_one(sql, &[])?;
+    let volk = Volk::from(row);
+    Ok(volk) // return a strukt
 }
 
 fn main() {
-    let test = database_query("select * from hives;").unwrap();
-    println!("{}", test);
-    println!();
-    let test2 = database_execute("select * from hives;").unwrap();
-    println!("{}", test2);
-    println!();
-    let test3 = database_test("SELECT id,volk,nummer,koenigin,erstellt::varchar,aufgeloest::varchar,typ,raehmchenmass, stand FROM volk").unwrap();
+    let test3 = database_fetchone("SELECT id,volk,nummer,koenigin,erstellt::varchar,aufgeloest::varchar,typ,raehmchenmass, stand FROM volk").unwrap();
     println!(
         "{} | {} | {} | {} | {} | {} | {} | {} | {} ",
         test3.id,
