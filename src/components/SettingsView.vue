d7d4c4ea9157b1ce42438813cec256a4cd95604b
kingkaos <da.cebulla@gmail.com>
2023-03-24 13:27:36 +0100

    feat: add db settings from store

--- src/components/SettingsView.vue
+++ src/components/SettingsView.vue
@@ -1,35 +1,47 @@
 <template>
   <div class="block">
-    {{ value2 }}
-    Settings are read from a settings file :)
     <div class="content">
-      <h2>General Settings</h2>
+      <!-- <h2>General Settings</h2> -->
+      <h2>Database</h2>
       <div class="level">
         <div class="level-item">
           <div class="label">
-            Path to settings file
+            Database url
           </div>
         </div>
         <div class="level-item">
           <div class="control">
-            <input class="input" type="text" placeholder="Text input">
+            <input class="input" type="text" placeholder="database url" :value="dbSettings.url">
           </div>
         </div>
       </div>
-      <h2>Database</h2>
-        <div class="columns">
-          <div class="column">
-            test
-          </div>
-          <div class="column">
-            sflkjh
+
+      <div class="level">
+        <div class="level-item">
+          <div class="label">
+            Database port
           </div>
         </div>
+        <div class="level-item">
+          <div class="control">
+          <input class="input" type="text" placeholder="database port" :value="dbSettings.port">
+          </div>
+        </div>
+      </div>
+
+      <div class="level">
+        <div class="level-item">
+          <div class="label">
+            Database user
+          </div>
+        </div>
+        <div class="level-item">
+          <div class="control">
+          <input class="input" type="text" placeholder="database user" :value="dbSettings.user">
+          </div>
+        </div>
+      </div>
     </div>
-    Settings
-  </div>
-  <div class="block">
-    Database
   </div>
 </template>
 
@@ -37,34 +49,13 @@
 // import { Store } from "tauri-plugin-store-api";
 import { ref } from "vue";
 import { initStore } from "../store/store.ts";
-const value2 = ref(0)
 
-// const store = ref(new Store(".settings.dat"));
-// // console.log('HERE')
-// console.log(store.value);
+const dbSettings = ref({})
 
-const test = await initStore()
-console.log(await test.get("some-key"))
-console.log(await test.get("a"))
-// async function testSet () {
-//   store.value.set("some-key", { value: 0 });
-// }
-//
-// await testSet();
-//
-// const newVal = await store.get("some-key");
-// console.log(newVal)
-//
-// store.set("some-key", { value: newVal.value + 1 });
-// const val = await store.get("some-key");
-// // assert(val, { value: 5 });
-// console.log(val);
-// async () => await store.save();
-// test()
-// console.log(store)
-// async function test () {
-//   const tt = await store.get("some-key");
-//   console.log(tt);
-// }
-//
+async function loadDbSettings () {
+  const store = await initStore()
+  dbSettings.value = await store.get("database")
+}
+
+await loadDbSettings()
 </script>
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,6 +2,10 @@ import { Store } from "tauri-plugin-store-api";
 
 export async function initStore () {
   const store = new Store(".settings.dat");
-  store.set("some-key", { value: 123 });
+  store.set("database", {
+    url: "database url",
+    port: "database port",
+    user: "database user",
+  });
   return store;
 }
