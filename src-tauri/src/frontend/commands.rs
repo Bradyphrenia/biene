d2a5b0429ab0f9902a1aa66f0a82e72ed6626068
kingkaos <da.cebulla@gmail.com>
2023-03-29 01:06:22 +0200

    feat: access database credetials from store

--- src-tauri/src/frontend/commands.rs
+++ src-tauri/src/frontend/commands.rs
@@ -1,3 +1,6 @@
+use tauri_plugin_store::{JsonValue, StoreCollection, with_store};
+use tauri::Wry;
+use std::path::PathBuf;
 use serde::{Serialize, Deserialize};
 
 #[tauri::command]
@@ -33,9 +36,34 @@ impl Default for Database {
     }
 }
 
+fn get_from_store(
+    app_handle: tauri::AppHandle,
+    stores: tauri::State<'_, StoreCollection<Wry>>,
+    store_key: &str
+) -> Result<std::option::Option<JsonValue>, tauri_plugin_store::Error> {
+    let path = PathBuf::from(".settings.dat");
+    with_store(app_handle, stores, path, |store| Ok(store.get(store_key).cloned()))
+}
+
+fn get_db_from_store(
+    app_handle: tauri::AppHandle,
+    stores: tauri::State<'_, StoreCollection<Wry>>
+) -> Database {
+    let mut db_settings: Database = Default::default();
+    match get_from_store(app_handle, stores, "database") {
+        Ok(res) => { db_settings = serde_json::from_value(res.clone().into()).unwrap(); },
+        Err(err) => { println!("{}", err); }
+    }
+    db_settings
+}
 
 #[tauri::command]
-pub fn connect_db() {
+pub fn connect_db(
+    app_handle: tauri::AppHandle,
+    stores: tauri::State<'_, StoreCollection<Wry>>
+) {
+    let db_settings = get_db_from_store(app_handle, stores);
+    // get db settings with db_settings.url ...
     // TODO: implement me
 }
 
