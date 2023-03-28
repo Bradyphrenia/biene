585e8146ce15b4bc58e1d4464edeb4965e3b1e61
kingkaos <da.cebulla@gmail.com>
2023-03-28 16:56:30 +0200

    feat: add database credential struct

--- src-tauri/src/frontend/commands.rs
+++ src-tauri/src/frontend/commands.rs
@@ -1,4 +1,50 @@
+use serde::{Serialize, Deserialize};
+
 #[tauri::command]
 pub fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Database {
+    url: String,
+    port: String,
+    database: String,
+    user: String,
+    password: String,
+}
+
+impl Database {
+    fn new() -> Self {
+        Default::default()
+    }
+}
+
+impl Default for Database {
+    fn default() -> Self {
+        Database {
+            url: String::from("127.0.0.1"),
+            port: String::from("5432"),
+            database: String::from("biene"),
+            user: String::from("postgres"),
+            password: String::from("postgres")
+        }
+    }
+}
+
+
+#[tauri::command]
+pub fn connect_db() {
+    // TODO: implement me
+}
+
+#[tauri::command]
+pub fn disconnect_db() {
+    // TODO: implement me
+}
+
+#[tauri::command]
+pub fn get_table_list(table_name: &str) {
+    // TODO: implement me
 }
