393231aac10318cc2dc85f59a83484bd8f1aed5a
kingkaos <da.cebulla@gmail.com>
2023-03-30 22:11:36 +0200

    feat: add basic input and configuration component

---
+++ src/core/Configuration.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="columns">
+    <div class="column">
+      {{ label }}
+    </div>
+    <div class="column control">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+})
+
+</script>
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { createApp } from "vue";
 
 import "./main.less";
 
+import Configuration from "./core/Configuration.vue";
+import Input from "./core/Input.vue";
+
 import App from "./App.vue";
 
 import router from "./router";
 
 createApp(App)
   .use(router)
+  .component("c-configuration", Configuration)
+  .component("c-input", Input)
   .mount("#app");
