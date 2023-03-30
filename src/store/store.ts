54fc25be94d1358851340f06d9566d0024f4aa30
kingkaos <da.cebulla@gmail.com>
2023-03-30 22:15:09 +0200

    feat: add empty store initializer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,39 @@
 import { Store } from "tauri-plugin-store-api";
 
-export async function initStore () {
-  const store = new Store(".settings.dat");
-  store.set("database", {
-    url: "database url",
-    port: "database port",
-    user: "database user",
-  });
-  return store;
+async function initEmptyStore (storeKey: string) {
+  const tauriStore = new Store(".settings");
+  if (!tauriStore.has(storeKey)) {
+    await tauriStore.set(storeKey, {});
+    await tauriStore.save();
+  }
+}
+
+// function initStore () {
+//     const ts = new Store(".settings.dat");
+//     ts.set("database", {
+//
+//             url: "127.0.0.1",
+//             port: "5432",
+//             database: "biene",
+//             user: "postgres",
+//             password: "postgres"
+//     })
+//     ts.save();
+// }
+
+export async function getTauriStore (storeKey: string) {
+  const tauriStore = new Store(".settings.dat");
+  if (!tauriStore.has(storeKey)) initEmptyStore(storeKey);
+
+  return await tauriStore.get(storeKey);
+}
+
+export async function setTauriStore (
+  storeKey: string,
+  data: object
+) {
+  console.log(data)
+  const tauriStore = new Store(".settings.dat");
+  await tauriStore.set(storeKey, data);
+  await tauriStore.save();
 }
