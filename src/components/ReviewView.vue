862d93d0d24879e0df87acd8dcd7ca56312888ec
kingkaos <da.cebulla@gmail.com>
2023-03-30 22:40:10 +0200

    feat: add table setup for Durchsicht

--- src/components/ReviewView.vue
+++ src/components/ReviewView.vue
@@ -1,5 +1,29 @@
 <template>
-  Review Page
+  <div class="p-6 lg:flex-1">
+    <table class="table is-narrow is-fullwidth is-hoverable is-striped">
+      <thead>
+        <tr>
+          <th>Datum</th>
+          <th>Volk</th>
+          <th>Königin</th>
+          <th>Stifte</th>
+          <th>Offene</th>
+          <th>Verdeckelte</th>
+          <th>Weiselzelle</th>
+          <th>Spielnäpfe</th>
+          <th>Sanftmut</th>
+          <th>Volksstärke</th>
+          <th># Brutwaben</th>
+          <th>Memo</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>...</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script setup lang="ts">
