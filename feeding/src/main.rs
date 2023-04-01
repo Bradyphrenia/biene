7cb5f99ff8f4400df63bd227dae6209d4e624ce0
steffen <stro889002@aol.com>
2023-04-01 17:38:51 +0200

    Refactor: structs of the various hive types

--- feeding/src/main.rs
+++ feeding/src/main.rs
@@ -5,15 +5,12 @@ use crate::feeding_mod::{
 };
 
 pub fn main() {
-    let testww = feeding_mod::WarreWeights::new();
+    let testww = WarreWeights::new();
     let testwc = WarreCounts::new();
     let wweight = netto_weight(testww, testwc, false);
     let bweight = brutto_weight(testww, testwc, false);
     let feedw = feed_need(bweight, 25.0);
-    println!("Nettogewicht der Warré-Beute ohne Fütterer: {}", wweight);
-    println!(
-        "Sollgewicht der Warré-Beute nach dem Einfüttern: {}",
-        bweight
-    );
-    println!("Futter noch zu geben bei Gewicht von 25 kg: {}", feedw)
+    println!("Nettogewicht der Warré-Beute ohne Fütterer: {wweight}");
+    println!("Sollgewicht der Warré-Beute ohne Fütterer nach dem Einfüttern: {bweight}");
+    println!("Futter notwendig bei einem aktuellen Gewicht von 25 kg: {feedw}")
 }
