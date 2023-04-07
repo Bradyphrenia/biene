98d006a2d635ed1e26d60fa5ecee1ac870baf25c
kingkaos <da.cebulla@gmail.com>
2023-04-07 22:18:37 +0200

    feat: separate tauri init from execute

--- src-tauri/src/frontend/builder.rs
+++ src-tauri/src/frontend/builder.rs
@@ -7,9 +7,11 @@ use super::commands;
 
 pub fn app () {
 
-    tauri::Builder::default()
+    let tauri_app = tauri::Builder::default()
 
-        .invoke_handler(tauri::generate_handler![commands::greet])
+        .invoke_handler(tauri::generate_handler![
+            commands::test_db_connection
+        ])
 
         .plugin(tauri_plugin_store::Builder::default().build())
         .setup(|app2| {
@@ -18,7 +20,7 @@ pub fn app () {
             let path = PathBuf::from(".settings.dat");
             let result = with_store(app_handle, stores, path, |store| store.insert("a".to_string(), json!({ "code": 200 })));
                     result.map_err(|error| Box::new(error) as Box<dyn std::error::Error>)
-        })
-        .run(tauri::generate_context!())
+        });
+    tauri_app.run(tauri::generate_context!())
         .expect("error while running tauri application")
 }
