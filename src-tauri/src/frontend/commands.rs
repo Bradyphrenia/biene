ef62c15dd76ec45bd577d749a13d573b0944c5bb
kingkaos <da.cebulla@gmail.com>
2023-04-07 22:26:08 +0200

    feat: add db connection test command

--- src-tauri/src/frontend/commands.rs
+++ src-tauri/src/frontend/commands.rs
@@ -32,11 +32,35 @@ fn get_db_from_store(
 }
 
 #[tauri::command]
-pub fn connect_db(
+pub fn test_db_connection(
     app_handle: tauri::AppHandle,
     stores: tauri::State<'_, StoreCollection<Wry>>
-) {
+) -> String {
+    // get credentials from tauri store
     let db_settings = get_db_from_store(app_handle, stores);
+    // postgresql://USER:PASSWORD@URL:PORT/DATABASE
+    let client = match Client::connect(
+        format!(
+            "postgresql://{}:{}@{}:{}/{}",
+            db_settings.user,
+            db_settings.password,
+            db_settings.url,
+            db_settings.port,
+            db_settings.database
+        ).as_str(), NoTls
+    ){
+        Ok(_) => format!("Success"),
+        Err(_e) => format!("Failed: {}", _e),
+    };
+    client
+}
+
+#[tauri::command]
+pub fn connect_db(
+    // app_handle: tauri::AppHandle,
+    // stores: tauri::State<'_, StoreCollection<Wry>>
+) {
+    // let db_settings = get_db_from_store(app_handle, stores);
     // get db settings with db_settings.url ...
     // TODO: implement me
 }
