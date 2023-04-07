07dc887b15349fb7301ff58251c91c9868b477f8
kingkaos <da.cebulla@gmail.com>
2023-04-07 22:23:30 +0200

    feat: comment unused tauri setup section
    Setup section might be useful for future extensions and thus it will be
    kept. At least for the next few steps :)

--- src-tauri/src/frontend/builder.rs
+++ src-tauri/src/frontend/builder.rs
@@ -13,14 +13,14 @@ pub fn app () {
             commands::test_db_connection
         ])
 
-        .plugin(tauri_plugin_store::Builder::default().build())
-        .setup(|app2| {
-            let app_handle = app2.handle().clone();
-            let stores = app2.state::<StoreCollection<Wry>>();
-            let path = PathBuf::from(".settings.dat");
-            let result = with_store(app_handle, stores, path, |store| store.insert("a".to_string(), json!({ "code": 200 })));
-                    result.map_err(|error| Box::new(error) as Box<dyn std::error::Error>)
-        });
+        .plugin(tauri_plugin_store::Builder::default().build());
+        // .setup(|app2| {
+        //     let app_handle = app2.handle().clone();
+        //     let stores = app2.state::<StoreCollection<Wry>>();
+        //     let path = PathBuf::from(".settings.dat");
+        //     let result = with_store(app_handle, stores, path, |store| store.insert("a".to_string(), json!({ "code": 200 })));
+        //             result.map_err(|error| Box::new(error) as Box<dyn std::error::Error>)
+        // });
     tauri_app.run(tauri::generate_context!())
         .expect("error while running tauri application")
 }
