519d8e9576728b299326c6569d583c7e11293e83
steffen <stro889002@aol.com>
2023-04-16 00:09:56 +0200

    new: start mini console app for data input

--- database/src/main.rs
+++ database/src/main.rs
@@ -1,16 +1,18 @@
+mod database;
+
 use crate::database::dbase::Durchsicht;
 use database::dbase::{
     db_execute, durchsicht_fetchall, durchsicht_fetchone, init_db, volk_fetchall, volk_fetchone,
 };
 
-mod database;
-
 // to input a string
 fn inp_str(prompt: &str) -> String {
     let mut line = String::new();
     println!("{}", prompt);
+
     std::io::stdin().read_line(&mut line).unwrap();
     let input = line.trim().to_string();
+    println!("{}", &input);
     input
 }
 
@@ -20,10 +22,13 @@ fn inp_bool(prompt: &str) -> bool {
     println!("{}", prompt);
     std::io::stdin().read_line(&mut line).unwrap();
     let input = line.trim().to_string();
-    match input.as_str() {
+    let bool_ = match input.as_str() {
         "1" => true,
+        "0" => false,
         _ => false,
-    }
+    };
+    println!("{}", &bool_);
+    bool_
 }
 
 // to input a number i16
@@ -31,8 +36,9 @@ fn inp_number(prompt: &str) -> i16 {
     let mut line = String::new();
     println!("{}", prompt);
     std::io::stdin().read_line(&mut line).unwrap();
-    let input = line.trim().to_string();
-    input.parse::<i16>().unwrap()
+    let input = line.trim().parse::<i16>().unwrap();
+    println!("{}", &input);
+    input
 }
 
 // console app
@@ -42,17 +48,17 @@ fn main() {
     ds.datum = input;
     let input = inp_str("Volk?  Volk 99");
     ds.volk = input;
-    let input = inp_bool("Königin?  1/0");
+    let input = inp_bool("Königin?  1 | 0");
     ds.koenigin = input;
-    let input = inp_bool("Stifte?  1/0");
+    let input = inp_bool("Stifte?  1 | 0");
     ds.stifte = input;
-    let input = inp_bool("offene Brut?  1/0");
+    let input = inp_bool("offene Brut?  1 | 0");
     ds.offene = input;
-    let input = inp_bool("verdeckelte Brut?  1/0");
+    let input = inp_bool("verdeckelte Brut?  1 | 0");
     ds.verdeckelte = input;
-    let input = inp_bool("Weiselzelle?  1/0");
+    let input = inp_bool("Weiselzelle?  1 | 0");
     ds.weiselzelle = input;
-    let input = inp_bool("Spielnäpfe?  1/0");
+    let input = inp_bool("Spielnäpfe?  1 | 0");
     ds.spielnaepfe = input;
     let input = inp_number("Sanftmut?  1 - 5");
     ds.sanftmut = input;
@@ -63,6 +69,7 @@ fn main() {
     let input = inp_str("Bemerkungen?");
     ds.memo = input;
     let sql = Durchsicht::ds_to_sql(&ds);
+    println!("{}", &sql);
     let db = init_db();
     let lines = db_execute(sql.as_str(), db);
     println!("Es wurde(n) {} Zeile(n) hinzugefügt.", lines);
