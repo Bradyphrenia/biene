25a700fe5421bdcbaa6d54d7ff2faaf4d09d4815
steffen <stro889002@aol.com>
2023-04-17 19:29:23 +0200

    new: start mini console app for data input

--- database/tests/cli.rs
+++ database/tests/cli.rs
@@ -1,4 +1,15 @@
+#[path = "../src/mod.rs"]
+mod database;
+
+use crate::database::database::dbase::Durchsicht;
+use crate::database::database::dbase::{
+    db_execute, durchsicht_fetchall, durchsicht_fetchone, init_db, volk_fetchall, volk_fetchone,
+};
+
 #[test]
-fn works(){
-    assert!(true);
+fn works() {
+    let db = init_db();
+    let sql: String = "SELECT * FROM durchsicht WHERE id = 155;".to_string(); // one dataset!!!
+    let lines = db_execute(sql.as_str(), db);
+    assert_eq!(lines, 1);
 }
