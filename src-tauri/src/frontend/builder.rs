3e68251c651c77fdf4b4f536478b2a7c662fcae6
kingkaos <da.cebulla@gmail.com>
2023-06-15 22:20:48 +0200

    refactor: move tauri app builder to main

--- src-tauri/src/frontend/builder.rs
+++ src-tauri/src/frontend/builder.rs
@@ -1,8 +1,9 @@
+// TODO: this file is outdated and should be removed in a future version
 use super::commands;
 
 pub fn app() {
     let tauri_app = tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![commands::test_db_connection])
+        .invoke_handler(tauri::generate_handler![commands::test_db_connection, commands::test_get])
         .plugin(tauri_plugin_store::Builder::default().build());
 
     tauri_app
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod frontend;
+// mod frontend;
 mod database;
 
-fn main() {
-    // connect to database
-    // TODO: implement me
+use crate::frontend::commands;
 
+fn app() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            commands::test_db_connection,
+            commands::test_get
+        ])
+        .plugin(tauri_plugin_store::Builder::default().build())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application")
+}
+
+fn main() {
     // start tauri application
     frontend::builder::app();
 }
