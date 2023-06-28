c83a9402f9ed1c2a5cbafd6551f8bdf5836349d5
kingkaos <da.cebulla@gmail.com>
2023-06-28 21:05:43 +0200

    chore: remove obsolet and commented code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,23 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod frontend;
-mod communication;
 mod commands;
+mod communication;
 mod database;
+mod frontend;
 mod util;
-
-
-// use crate::frontend::commands;
-use crate::commands as other_commands;
 
 fn app() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-            // commands::test_db_connection,
-            // commands::test_get,
-            other_commands::connect_to_db,
-        ])
+        .invoke_handler(tauri::generate_handler![commands::connect_to_db,])
         .plugin(tauri_plugin_store::Builder::default().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application")
