5eadc4e8cdc6b2c606e05f9743a947036f5788a9
steffen <stro889002@aol.com>
2023-07-05 19:42:07 +0200

    feature: small command line interface for adding a dataset to the table "durchsicht"

--- queen/src/main.rs
+++ queen/src/main.rs
@@ -1,8 +1,8 @@
 use chrono::{Duration, NaiveDate};
 
 fn main() {
-    let dt = NaiveDate::from_ymd_opt(2023, 7, 3).unwrap();
+    let dt = NaiveDate::from_ymd_opt(2023, 7, 2).unwrap();
     println!("Caging cells on {}.", dt + Duration::days(5));
-    println!("Transferring cells on {}.", dt + Duration::days(10));
+    println!("Transferring cells allowed on {}.", dt + Duration::days(10));
     println!("Queens emerging on {}.", dt + Duration::days(11));
 }
