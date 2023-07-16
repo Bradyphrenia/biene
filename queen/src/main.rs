34b0a97dc4d2ac6700e5fde7e22eba3310be003c
steffen <stro889002@aol.com>
2023-07-16 13:04:31 +0200

    feature: small command line interface for calculating queen raring dates

--- queen/src/main.rs
+++ queen/src/main.rs
@@ -1,10 +1,17 @@
 use chrono::{Duration, NaiveDate};
 
 fn main() {
-    let dt = NaiveDate::from_ymd_opt(2023, 7, 2).unwrap();
-    println!("Caging cells on {}.", dt + Duration::days(5));
-    println!("Transferring cells allowed on {}.", dt + Duration::days(10));
-    println!("Queens emerging on {}.", dt + Duration::days(11));
+    let breeding = queen_raring(2023, 7, 2);
+    println!("Caging cells on {}.", breeding.0);
+    println!("Transferring cells allowed on {}.", breeding.1);
+    println!("Queens emerging on {}.", breeding.2);
 }
 
- 
+fn queen_raring(year: i32, month: u32, day: u32) -> (NaiveDate, NaiveDate, NaiveDate) {
+    let dt = NaiveDate::from_ymd_opt(year, month, day).unwrap();
+    (
+        dt + Duration::days(5),
+        dt + Duration::days(10),
+        dt + Duration::days(11),
+    )
+}
