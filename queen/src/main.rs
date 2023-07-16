a8569e42d145f5402b1d612f6f1c3894a8740d0b
steffen <stro889002@aol.com>
2023-07-16 14:15:43 +0200

    feature: small command line interface for calculating queen raring dates

--- queen/src/main.rs
+++ queen/src/main.rs
@@ -9,13 +9,16 @@ fn main() {
 
 fn queen_raring(year: i32, month: u32, day: u32) -> (NaiveDate, NaiveDate, NaiveDate) {
     let dt = match NaiveDate::from_ymd_opt(year, month, day) {
-        Some(dt) =>
-            (
-                dt + Duration::days(5), //Caging cells on
-                dt + Duration::days(10), //Transferring cells allowed on
-                dt + Duration::days(11), //Queens emerging on
-            ),
-        None => (NaiveDate::parse_from_str("2020-1-1", "%Y-%m-%d").unwrap(), NaiveDate::parse_from_str("2020-1-1", "%Y-%m-%d").unwrap(), NaiveDate::parse_from_str("2020-1-1", "%Y-%m-%d").unwrap())
+        Some(dt) => (
+            dt + Duration::days(5),  //Caging cells on
+            dt + Duration::days(10), //Transferring cells allowed on
+            dt + Duration::days(11), //Queens emerging on
+        ),
+        None => (
+            NaiveDate::parse_from_str("2020-1-1", "%Y-%m-%d").unwrap(), // in case of error
+            NaiveDate::parse_from_str("2020-1-1", "%Y-%m-%d").unwrap(),
+            NaiveDate::parse_from_str("2020-1-1", "%Y-%m-%d").unwrap(),
+        ),
     };
     dt
 }
