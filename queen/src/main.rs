a34a679e1d9eb61dd0edac7ae55478167a79f3d4
steffen <stro889002@aol.com>
2023-07-23 19:24:56 +0200

    add: queen raring calendar

--- queen/src/main.rs
+++ queen/src/main.rs
@@ -1,24 +1,36 @@
 use chrono::{Duration, NaiveDate};
 
-fn main() {
-    let breeding = queen_raring(2023, 7, 2);
-    println!("Caging cells on {}.", breeding.0);
-    println!("Transferring cells allowed on {}.", breeding.1);
-    println!("Queens emerging on {}.", breeding.2);
+struct QueenRaring {
+    larva: NaiveDate,
+    caging: NaiveDate,
+    transfer: NaiveDate,
+    emerge: NaiveDate,
 }
 
-fn queen_raring(year: i32, month: u32, day: u32) -> (NaiveDate, NaiveDate, NaiveDate) {
-    let dt = match NaiveDate::from_ymd_opt(year, month, day) {
-        Some(dt) => (
-            dt + Duration::days(5),  //Caging cells on
-            dt + Duration::days(10), //Transferring cells allowed on
-            dt + Duration::days(11), //Queens emerging on
-        ),
-        None => (
-            NaiveDate::parse_from_str("2020-1-1", "%Y-%m-%d").unwrap(), // in case of error
-            NaiveDate::parse_from_str("2020-1-1", "%Y-%m-%d").unwrap(),
-            NaiveDate::parse_from_str("2020-1-1", "%Y-%m-%d").unwrap(),
-        ),
+fn main() {
+    let mut breeding = QueenRaring {
+        larva: NaiveDate::parse_from_str("2020-1-1", "%Y-%m-%d").unwrap(),
+        caging: NaiveDate::parse_from_str("2020-1-1", "%Y-%m-%d").unwrap(),
+        transfer: NaiveDate::parse_from_str("2020-1-1", "%Y-%m-%d").unwrap(),
+        emerge: NaiveDate::parse_from_str("2020-1-1", "%Y-%m-%d").unwrap(),
     };
-    dt
+    breeding.queen_raring(2023, 7, 2);
+    println!("Started series on {}.", breeding.larva);
+    println!("Caging cells on {}.", breeding.caging);
+    println!("Transferring cells allowed on {}.", breeding.transfer);
+    println!("Queens emerging on {}.", breeding.emerge);
+}
+
+impl QueenRaring {
+    fn queen_raring(&mut self, year: i32, month: u32, day: u32) -> () {
+        match NaiveDate::from_ymd_opt(year, month, day) {
+            Some(dt) => {
+                self.larva = dt;
+                self.caging = dt + Duration::days(5); //Caging cells on
+                self.transfer = dt + Duration::days(10); //Transferring cells allowed on
+                self.emerge = dt + Duration::days(11); //Queens emerging on
+            }
+            None => (todo!()),
+        };
+    }
 }
