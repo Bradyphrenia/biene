f00b14af2faaac25263a20a0bce647ea68a80629
steffen <stro889002@aol.com>
2023-09-16 19:46:25 +0200

    feature: small command line interface for calculating queen raring dates

--- feeding/src/main.rs
+++ feeding/src/main.rs
@@ -3,6 +3,7 @@ use feeding::calculations::{
     brutto_weight, feed_need, feed_present, netto_weight, SetFeeder, SetZargeCount, WarreCounts,
     WarreWeights,
 };
+use crate::feeding::calculations::{DadantCounts, DadantWeights, NormalmassCounts, NormalmassWeights};
 
 pub fn main() {
     let testww = WarreWeights::new();
@@ -10,11 +11,21 @@ pub fn main() {
     testwc = testwc.set_zarge_count(3).set_feeder(false);
     dbg!(&testwc);
     let wweight = netto_weight(testww, testwc);
-    let fw = feed_present(testww, testwc, 28.0);
+    let fw = feed_present(testww, testwc, 35.0);
     let bweight = brutto_weight(testww, testwc);
-    let feedw = feed_need(bweight, 28.0);
+    let feedw = feed_need(bweight, 35.0);
     println!("Nettogewicht der Warré-Beute ohne Fütterer: {wweight} kg");
     println!("Futter noch in der Warré-Beute ohne Fütterer: {fw} kg");
     println!("Sollgewicht der Warré-Beute ohne Fütterer nach dem Einfüttern: {bweight} kg");
-    println!("Futter notwendig bei einem aktuellen Gewicht von 28 kg: {feedw} Liter 1:1")
+    println!("Futter notwendig bei einem aktuellen Gewicht von 28 kg: {feedw} Liter 1:1");
+    let testdd = DadantWeights::new();
+    let mut testddc = DadantCounts::new();
+    testddc = testddc.set_feeder(false);
+    let bddsw = brutto_weight(testdd,testddc);
+    println!("Sollgewicht der Dadant-Beute ohne Fütterer nach dem Einfüttern: {bddsw} kg");
+    let testdn = NormalmassWeights::new();
+    let mut testdnc = NormalmassCounts::new();
+    testdnc = testdnc.set_feeder(false);
+    let bdnsw = brutto_weight(testdn,testdnc);
+    println!("Sollgewicht der Normalmaß-Beute ohne Fütterer nach dem Einfüttern: {bdnsw} kg");
 }
