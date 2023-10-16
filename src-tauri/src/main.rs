7ceaaaa92228aed019ff364e3bd336b44a6d6254
kingkaos <da.cebulla@gmail.com>
2023-10-16 20:06:55 +0200

    lint: apply cargo fmt

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{App, CustomMenuItem, Menu, MenuItem, Submenu, Manager, WindowBuilder, AboutMetadata};
+use tauri::{AboutMetadata, App, CustomMenuItem, Manager, Menu, MenuItem, Submenu, WindowBuilder};
 
 mod commands;
 mod communication;
 mod database;
+mod diesel;
 mod frontend;
 mod util;
-mod diesel;
 
 fn init_menu() -> Menu {
     let settings = CustomMenuItem::new("settings".to_string(), "Settings...");
@@ -18,10 +18,9 @@ fn init_menu() -> Menu {
             .add_native_item(MenuItem::About("Bees".to_string(), AboutMetadata::new()))
             .add_item(settings)
             .add_native_item(MenuItem::CloseWindow)
-            .add_native_item(MenuItem::Quit)
+            .add_native_item(MenuItem::Quit),
     );
-    Menu::new()
-        .add_submenu(submenu)
+    Menu::new().add_submenu(submenu)
 }
 
 fn init_app() -> App {
@@ -32,25 +31,25 @@ fn init_app() -> App {
             let handle = app.handle();
             let window = main_window.clone();
 
-            main_window.on_menu_event(move |event| {
-                match event.menu_item_id() {
-                    "settings" => {
-                        WindowBuilder::new(
-                            &handle,
-                            "settings",
-                            tauri::WindowUrl::App("#/settings".into())
-                        )
-                            .title("settings")
-                            .build()
-                            .unwrap();
-                    }
-                    _ => {}
+            main_window.on_menu_event(move |event| match event.menu_item_id() {
+                "settings" => {
+                    WindowBuilder::new(
+                        &handle,
+                        "settings",
+                        tauri::WindowUrl::App("#/settings".into()),
+                    )
+                    .title("settings")
+                    .build()
+                    .unwrap();
                 }
+                _ => {}
             });
             Ok(())
-
         })
-        .invoke_handler(tauri::generate_handler![commands::connect_to_db, commands::test,])
+        .invoke_handler(tauri::generate_handler![
+            commands::connect_to_db,
+            commands::test,
+        ])
         .plugin(tauri_plugin_store::Builder::default().build())
         .build(tauri::generate_context!())
         .expect("error while running tauri application")
