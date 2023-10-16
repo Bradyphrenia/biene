7bf1cfe7cfe308ecafd0561d7c7272a28863fbbe
kingkaos <da.cebulla@gmail.com>
2023-10-16 14:39:49 +0200

    feat: add settings window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{App, CustomMenuItem, Menu, MenuItem, Submenu, Manager, WindowBuilder, AboutMetadata};
 
 mod commands;
 mod communication;
 mod database;
 mod frontend;
 mod util;
+mod diesel;
 
 fn init_menu() -> Menu {
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
@@ -25,16 +27,42 @@ fn init_menu() -> Menu {
         .add_submenu(submenu)
 }
 
-fn app() {
-    tauri::Builder::default()
+fn app() -> App {
+    let app = tauri::Builder::default()
         .menu(init_menu())
+        .setup(|app| {
+            let main_window = app.get_window("main").unwrap();
+            let handle = app.handle();
+            let window = main_window.clone();
+
+            main_window.on_menu_event(move |event| {
+                match event.menu_item_id() {
+                    "settings" => {
+                        WindowBuilder::new(
+                            &handle,
+                            "settings",
+                            tauri::WindowUrl::App("#/settings".into())
+                        )
+                            .title("settings")
+                            .build()
+                            .unwrap();
+                    }
+                    _ => {}
+                }
+            });
+            Ok(())
+
+        })
         .invoke_handler(tauri::generate_handler![commands::connect_to_db, commands::test,])
         .plugin(tauri_plugin_store::Builder::default().build())
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application")
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application");
+        // .run(tauri::generate_context!())
+    app
 }
 
 fn main() {
     // Let's get started :)
-    app();
+    let tt = app();
+    tt.run(|_, _| {})
 }
