0fdf4548330be9577dbe9101f8b2ba86a0b45d7c
kingkaos <da.cebulla@gmail.com>
2023-10-16 22:29:14 +0200

    feat: add diesel implementation

---
+++ src-tauri/src/diesel/models.rs
@@ -0,0 +1,62 @@
+use diesel::prelude::*;
+use diesel::sql_types::SmallInt;
+use serde::{ser::SerializeStruct, Deserialize, Serialize};
+#[derive(Serialize, Deserialize)]
+pub enum DatabaseTable {
+    Durchsicht,
+}
+
+// #[derive(Queryable, Selectable)]
+// #[diesel(table_name = crate::diesel::schema::posts)]
+// #[diesel(check_for_backend(diesel::pg::Pg))]
+// pub struct Post {
+//     pub id: i32,
+//     pub title: String,
+//     pub body: String,
+//     pub published: bool,
+// }
+
+#[derive(Queryable, Selectable, Debug)]
+#[diesel(table_name = crate::diesel::schema::durchsicht)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Durchsicht {
+    pub id: i32,
+    // #[serde(with = "PgDateDef")]
+    pub datum: Option<diesel::pg::data_types::PgDate>,
+    pub volk: Option<String>,
+    pub koenigin: Option<bool>,
+    pub stifte: Option<bool>,
+    pub offene: Option<bool>,
+    pub verdeckelte: Option<bool>,
+    pub weiselzelle: Option<bool>,
+    pub spielnaepfe: Option<bool>,
+    pub sanftmut: Option<i16>,
+    pub volksstaerke: Option<i16>,
+    pub anz_brutwaben: Option<i16>,
+}
+
+impl serde::Serialize for Durchsicht {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        println!("{:?}, {:?}", &self.datum, &self.koenigin);
+        let mut s = serializer.serialize_struct("Durchsicht", 1)?;
+        s.serialize_field("id", &self.id)?;
+        // s.serialize_field("datum", &self.datum)?;
+        s.serialize_field("koenigin", &self.koenigin)?;
+        s.end()
+    }
+}
+
+// #[derive(Serialize, Deserialize)]
+// #[serde(remote = "PgDate")]
+// struct PgDateDef ( i32 );
+
+// impl serde::Serialize for diesel::pg::data_types::PgDate {
+//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//     where
+//         S: serde::Serializer {
+//         serde_json::to_string(&self)
+//     }
+// }
---
+++ src-tauri/src/diesel/schema.rs
@@ -0,0 +1,30 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    durchsicht (id) {
+        id -> Int4,
+        datum -> Nullable<Date>,
+        #[max_length = 30]
+        volk -> Nullable<Varchar>,
+        koenigin -> Nullable<Bool>,
+        stifte -> Nullable<Bool>,
+        offene -> Nullable<Bool>,
+        verdeckelte -> Nullable<Bool>,
+        weiselzelle -> Nullable<Bool>,
+        spielnaepfe -> Nullable<Bool>,
+        sanftmut -> Nullable<Int2>,
+        volksstaerke -> Nullable<Int2>,
+        anz_brutwaben -> Nullable<Int2>,
+    }
+}
+
+diesel::table! {
+    posts (id) {
+        id -> Int4,
+        title -> Varchar,
+        body -> Text,
+        published -> Bool,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(durchsicht, posts,);
