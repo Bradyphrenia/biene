99d1e8244cfd23b3055f61522a7a3e660e3c4f07
kingkaos <da.cebulla@gmail.com>
2023-10-21 00:19:17 +0200

    feat: remove access to settings section from main
    Since this is a desktop application, settings is accessible similar to
    other desktop applications using menu.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
-  <!-- TODO: highlight active tab -->
-  <div class="tabs">
-    <ul>
-      <!-- <li><router-link to="/test">Greet</router-link></li> -->
-      <li><router-link to="/review">Durchsicht</router-link></li>
-      <li><router-link to="/settings">Einstellungen</router-link></li>
-    </ul>
-  </div>
+  <router-link to="/" />
+  <router-link to="/settings" />
   <Suspense>
     <router-view/>
   </Suspense>
