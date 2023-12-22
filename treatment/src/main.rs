8d3d8a7810e70aeb91f1c9fa8e7bddc431c28011
steffen <stro889002@aol.com>
2023-12-22 18:49:50 +0100

    feature: small command line interface for adhoc treatment of all hives with oxalic acid

--- treatment/src/main.rs
+++ treatment/src/main.rs
@@ -9,7 +9,7 @@ pub fn main() {
     let folks = vec![
         "Volk 01", "Volk 02", "Volk 03", "Volk 05", "Volk 06", "Volk 08", "Volk 09", "Volk 11",
     ];
-    let sql_1 = "INSERT INTO behandlung ( datum, volk, koenigin, stifte, offene, verdeckelte, weiselzelle, spielnaepfe, sanftmut, volksstaerke, anz_brutwaben, memo, krankheit, behandlung, anwendungsform, menge, mengeneinheit, konzentration, wartezeit, zulassungsnummer, meldepflicht) VALUES ('";
+    let sql_1 = "INSERT INTO behandlung (datum, volk, koenigin, stifte, offene, verdeckelte, weiselzelle, spielnaepfe, sanftmut, volksstaerke, anz_brutwaben, memo, krankheit, behandlung, anwendungsform, menge, mengeneinheit, konzentration, wartezeit, zulassungsnummer, meldepflicht) VALUES ('";
     let sql_2 = "', '";
     let sql_3 = "', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'VarroMed', 'geträufelt', 10, 'ml', NULL, NULL, NULL, NULL);";
     let date = input_date("Datum der Behandlung: ");
