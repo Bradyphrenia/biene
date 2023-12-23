34e1432c751e845d5c6564feb1e1f4b80add1b5a
steffen <stro889002@aol.com>
2023-12-23 15:28:49 +0100

    feature: small command line interface for adhoc treatment of all hives with oxalic acid

--- treatment/src/main.rs
+++ treatment/src/main.rs
@@ -4,23 +4,29 @@ mod database;
 use crate::cli::cli::input_date;
 use crate::database::database::db_execute;
 use crate::database::database::init_db;
+use postgres::Client;
+
+const HIVE_NAMES: [&str; 8] = [
+    "Volk 01", "Volk 02", "Volk 03", "Volk 04", "Volk 05", "Volk 06", "Volk 07", "Volk 08",
+];
+fn create_sql(date: &str, hive: &str) -> String {
+    return format!( "INSERT INTO behandlung (datum, volk, koenigin, stifte, offene, verdeckelte, weiselzelle, spielnaepfe, sanftmut, volksstaerke, anz_brutwaben, memo, krankheit, behandlung, anwendungsform, menge, mengeneinheit, konzentration, wartezeit, zulassungsnummer, meldepflicht) VALUES ('{}', '{}', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'VarroMed', 'geträufelt', 10, 'ml', NULL, NULL, NULL, NULL);", date, hive);
+}
+
+fn process_hive(hive: &str, date: String, db: Client) {
+    let sql = create_sql(&date, hive);
+    println!("{}", &sql);
+    let exec_result = db_execute(sql.as_str(), db);
+    if exec_result == 1 {
+        println!("Done.");
+    } else {
+        println!("Error.");
+    }
+}
 
 pub fn main() {
-    let hives = vec![
-        "Volk 01", "Volk 02", "Volk 03", "Volk 05", "Volk 06", "Volk 08", "Volk 09", "Volk 11",
-    ];
-    let sql_1 = "INSERT INTO behandlung (datum, volk, koenigin, stifte, offene, verdeckelte, weiselzelle, spielnaepfe, sanftmut, volksstaerke, anz_brutwaben, memo, krankheit, behandlung, anwendungsform, menge, mengeneinheit, konzentration, wartezeit, zulassungsnummer, meldepflicht) VALUES ('";
-    let sql_2 = "', '";
-    let sql_3 = "', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'VarroMed', 'geträufelt', 10, 'ml', NULL, NULL, NULL, NULL);";
     let date = input_date("Datum der Behandlung:");
-    for hive in &hives {
-        let sql = format!("{}{}{}{}{}", sql_1, date, sql_2, hive, sql_3);
-        println!("{}", &sql);
-        let test = db_execute(sql.as_str(), init_db());
-        if test == 1 {
-            println!("Done.");
-        } else {
-            println!("Error.");
-        }
+    for &hive in &HIVE_NAMES {
+        process_hive(hive, date.to_string(), init_db());
     }
 }
