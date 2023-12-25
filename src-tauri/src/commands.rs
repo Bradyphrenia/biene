83ce087394e5e95593debcd50a281b7736e9c0e3
kingkaos <da.cebulla@gmail.com>
2023-12-25 21:57:56 +0100

    fix: add missing commands file

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -7,6 +7,7 @@ use crate::util::store::get_db_from_store;
 use crate::util::structs::{Database, TauriCode, TauriResponse};
 
 use crate::diesel::models::{self, *};
+// use crate::diesel::schema::posts::dsl::*;
 use crate::diesel::schema::durchsicht::dsl::*;
 
 use diesel::prelude::*;
@@ -43,7 +44,27 @@ pub fn connect_to_db(
     }
 }
 
-/// Reading durchsicht table from database and providing results to frontend
+pub fn connect(
+    app_handle: tauri::AppHandle,
+    store: tauri::State<'_, StoreCollection<Wry>>,
+) -> PgConnection {
+    let Database {
+        user,
+        password,
+        url,
+        port,
+        database,
+    } = get_db_from_store(app_handle, store);
+
+    // TODO: add tauri store content
+    let connection = PgConnection::establish(&format!(
+        "postgres://{}:{}@{}:{}/{}",
+        user, password, url, port, database,
+    ))
+    .unwrap_or_else(|_| panic!("Error connecting to {}", url));
+    connection
+}
+
 #[tauri::command]
 pub fn request_review(
     app_handle: tauri::AppHandle,
@@ -58,6 +79,11 @@ pub fn request_review(
 
     // TODO: Return Object with status number
     Ok(results)
+}
+
+#[tauri::command]
+pub fn get_durchsicht(app_handle: tauri::AppHandle, store: tauri::State<'_, StoreCollection<Wry>>) {
+    let connection = &mut connect(app_handle, store);
 }
 
 #[tauri::command]
