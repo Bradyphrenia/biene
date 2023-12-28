dce377a62c788a5456e4ce7134d11a4edfd6507f
kingkaos <da.cebulla@gmail.com>
2023-12-28 21:28:58 +0100

    feat: add logging

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use log::{info, LevelFilter};
+use simple_logger::SimpleLogger;
+
 mod app;
 mod commands;
 mod communication;
@@ -12,6 +15,12 @@ mod util;
 use app::init_app;
 
 fn main() {
+    SimpleLogger::new()
+        .with_level(LevelFilter::Debug)
+        .env()
+        .init()
+        .unwrap();
+    info!("Starting biene");
     let app = init_app();
     app.run(|_, _| {})
 }
