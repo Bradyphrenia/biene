4863d9d456175f4653ca1282f75c07bfeb9c8199
steffen <stro889002@aol.com>
2024-01-07 14:12:16 +0100

    refactor: loop for all types

--- cli/feeding/src/main.rs
+++ cli/feeding/src/main.rs
@@ -5,20 +5,22 @@ use crate::hivetypes::calculations::{
 
 mod hivetypes;
 
+/// Main function that calculates and prints various hive information for different types of hives.
 pub fn main() {
+    let weight = 28.0;
     for type_ in [Types::warre, Types::dadant, Types::deutschnormal] {
         let mut test_hive = HiveTypes::new(type_);
         let name = test_hive.return_hive_name();
         test_hive.set_feeder(false); // no feeder
-        test_hive.set_current_weight(28.0); // set current weight to 28 kg
+        test_hive.set_current_weight(weight); // set current weight to ... kg
         let weight = test_hive.netto_weight();
         println!("Nettogewicht der {name}-Beute ohne Fütterer: {weight} kg");
-        let present = test_hive.feed_present(); // calculate the amount of feed actual present
+        let present = test_hive.feed_present(); // calculate the amount of feed actually present
         println!("Futter noch in der {name}-Beute ohne Fütterer: {present} kg");
-        let brutto = test_hive.brutto_weight(); // calculate what a hive should weight if already full
+        let brutto = test_hive.brutto_weight(); // calculate what a hive should weight if already ready for wintering
         println!("Sollgewicht der {name}-Beute ohne Fütterer nach dem Einfüttern: {brutto} kg");
         let need = test_hive.feed_need(); // calculate the amount of sugar sirup 1:1
-        println!("Futter notwendig bei einem aktuellen Gewicht von 28 kg: {need} Liter 1:1");
+        println!("Futter notwendig bei einem aktuellen Gewicht von {weight} kg: {need} Liter 1:1");
         println!();
     }
 }
